f89366aa0b397e5a7959f700fbe6c3dcd07cc05a
Mario Saliba <mario.saliba98@gmail.com>
2022-10-29 15:51:38 +0300

    resolve conflict

--- src/components/ContactUs/ContactUs.vue
+++ src/components/ContactUs/ContactUs.vue
@@ -3,45 +3,65 @@
     <div class="h-[100vh] flex justify-center items-center">
       <div class="contact-card">
         <div class="card-title">Contact Us</div>
-        <div
-          class="flex flex-col justify-center items-center space-y-3 text-[25px]"
-        >
-          <div class="flex flex-col justify-center w-[25rem]">
-            <div>Name</div>
-            <input v-model="name" class="name-input" :class="nameIsEmpty" />
-          </div>
-          <div class="flex flex-col justify-center w-[25rem]">
-            <div>Email</div>
-            <input v-model="email" class="email-input" :class="emailIsEmpty" />
-          </div>
-          <div class="flex flex-col justify-center w-[25rem]">
-            <div>Tell us how we can help</div>
-            <textarea
-              v-model="message"
-              class="message-input"
-              :class="messageIsEmpty"
+        <form class="card-body" @submit.prevent @submit="openModal">
+          <div class="mb-6">
+            <label
+              for="first_name"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="first_name"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="John"
+              required
+              v-model="name"
             />
           </div>
-        </div>
-        <div class="flex justify-center pt-3">
+          <div class="mb-6">
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="john.doe@company.com"
+              required
+              v-model="email"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              for="message"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows="4"
+              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Your message..."
+              required
+              v-model="message"
+            >
+            </textarea>
+          </div>
           <button
-            @click="openModal()"
             data-modal-target="#modal"
             class="submit-button"
+            type="submit"
           >
             Submit
           </button>
-        </div>
+        </form>
       </div>
-      <div class="modal" id="modal">
-        <div class="modal-text">
-          We got your request and we'll get back to you as soon as possible.
-        </div>
-        <div data-close-button class="ok-button-container">
-          <button @click="closeModal()" class="ok-button">Ok</button>
-        </div>
-      </div>
-      <div id="overlay"></div>
     </div>
   </div>
 </template>
@@ -53,52 +73,11 @@ export default {
       name: '',
       email: '',
       message: '',
-      modal: document.querySelector('.modal'),
-      overlay: document.getElementById('overlay'),
-      nameIsEmpty: '',
-      emailIsEmpty: '',
-      messageIsEmpty: '',
     }
   },
   methods: {
-    openModal: function () {
-      if (!this.name || !this.email || !this.message) {
-        if (!this.name) {
-          this.nameIsEmpty = 'errorEmpty'
-        } else {
-          this.nameIsEmpty = 'notEmpty'
-        }
-        if (!this.email) {
-          this.emailIsEmpty = 'errorEmpty'
-        } else {
-          if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email))
-            this.emailIsEmpty = 'notEmpty'
-          else this.emailIsEmpty = 'errorEmpty'
-        }
-        if (!this.message) {
-          this.messageIsEmpty = 'errorEmpty'
-        } else {
-          this.messageIsEmpty = 'notEmpty'
-        }
-        alert('You must fill out all the boxes')
-      } else {
-        this.nameIsEmpty = 'notEmpty'
-        this.emailIsEmpty = 'notEmpty'
-        this.messageIsEmpty = 'notEmpty'
-
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
-          modal.classList.add('active')
-          overlay.classList.add('active')
-        } else {
-          alert('You need to enter a valid email address')
-          this.emailIsEmpty = 'errorEmpty'
-        }
-      }
-    },
-    closeModal: function () {
-      modal.classList.remove('active')
-      overlay.classList.remove('active')
-    },
+    openModal: function () {},
+    closeModal: function () {},
   },
 }
 </script>
