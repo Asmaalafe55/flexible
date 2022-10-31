3d4efe6710149576a9018274e9474e4d88c475d3
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-31 21:49:48 +0200

    editing the template page

--- src/views/temps/TemplateCard.vue
+++ src/views/temps/TemplateCard.vue
@@ -4,7 +4,8 @@
       class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
     >
       <a href="#">
-        <img class="rounded-t-lg" src="../../assets/img/t2.png" alt="" />
+        <img class="rounded-t-lg" src="@/assets/img/t2.png" alt="" />
+        <!-- :src="'../../assets/img/' + template.imgPath" -->
       </a>
       <div class="p-5">
         <a href="#">
@@ -21,7 +22,7 @@
           href="#"
           class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Read more
+          View
           <svg
             aria-hidden="true"
             class="ml-2 -mr-1 w-4 h-4"
