a70f178c8eeb8b0fa06a1681a6fed8fa8e3f3b4d
Mario Saliba <mario.saliba98@gmail.com>
2022-11-09 15:16:38 +0200

    makes the homepage cards clickable

--- src/components/QuestionnaireOrTemplates/QuestionnaireOrTemplates.vue
+++ src/components/QuestionnaireOrTemplates/QuestionnaireOrTemplates.vue
@@ -7,12 +7,16 @@
     <div class="questionnaire-or-templates-container" id="wantSite">
       <div class="template-option">
         <div class="option-title text-white">Build your own website</div>
-        <div class="choose-card"></div>
+        <router-link :to="{ path: '/questionnaire' }">
+          <div class="choose-card"></div>
+        </router-link>
       </div>
       <div class="or-container">OR</div>
       <div class="template-option">
         <div class="option-title">Choose a template</div>
-        <div class="choose-card"></div>
+        <router-link :to="{ path: '/templates' }">
+          <div class="choose-card"></div>
+        </router-link>
       </div>
     </div>
   </div>
