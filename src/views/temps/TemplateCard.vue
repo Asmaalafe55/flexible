9fd2769ffdae34449b2e4aaf5dd033b2cdf181c3
Mario Saliba <mario.saliba98@gmail.com>
2022-11-12 18:08:46 +0200

    makes template list mobile compatible

--- src/views/temps/TemplateCard.vue
+++ src/views/temps/TemplateCard.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="relative">
-    <div
-      class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-    >
+  <div class="relative my-2 mx-2">
+    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
       <a href="#">
         <img
           class="rounded-t-lg h-[25vh] w-full"
@@ -12,9 +10,7 @@
       </a>
       <div class="p-5">
         <a href="#">
-          <h5
-            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-          >
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
             {{ template.template_name }}
           </h5>
         </a>
@@ -23,7 +19,7 @@
         </p>
         <a
           href="#"
-          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
         >
           View
           <svg
--- src/views/temps/Templates.vue
+++ src/views/temps/Templates.vue
@@ -1,114 +1,110 @@
 <template>
   <div class="parallax">
     <img class="parallax-img" src="../../assets/img/bg.png" />
-    <div class="min-h-screen">
-      <div class="relative sm:rounded-lg">
-        <div class="flex justify-between items-center h-16 m-2 ml-14 mx-8">
-          <div class="flex space-x-2">
-            <button
-              id="dropdownCheckboxButton"
-              data-dropdown-toggle="dropdownDefaultCheckbox"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-              type="button"
+    <div class="min-h-screen mb-4">
+      <div class="flex items-center flex-wrap m-2 lg:ml-8 lg:mx-8 mt-4">
+        <!-- Search -->
+        <div class="relative lg:mr-4">
+          <div
+            class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+          >
+            <svg
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Filter
-              <svg
-                class="ml-2 w-4 h-4"
-                aria-hidden="true"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 9l-7 7-7-7"
-                ></path>
-              </svg>
-            </button>
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            v-model="search"
+            class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Search for templates"
+          />
+        </div>
 
-            <!-- Dropdown menu -->
-            <div
-              id="dropdownDefaultCheckbox"
-              class="hidden z-10 w-48 bg-white rounded divide-y divide-gray-100 shadow"
-              data-popper-reference-hidden=""
-              data-popper-escaped=""
-              data-popper-placement="bottom"
-              style="
-                position: absolute;
-                inset: 0px auto auto 0px;
-                margin: 0px;
-                transform: translate3d(0px, 320px, 0px);
-              "
+        <div class="flex space-x-2 mt-2 lg:mt-0">
+          <button
+            id="dropdownCheckboxButton"
+            data-dropdown-toggle="dropdownDefaultCheckbox"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
+            type="button"
+          >
+            Filter
+            <svg
+              class="ml-2 w-4 h-4"
+              aria-hidden="true"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <ul
-                class="p-3 space-y-3 text-sm text-gray-700"
-                aria-labelledby="dropdownCheckboxButton"
-              >
-                <li v-for="check in dropdown">
-                  <div class="flex items-center">
-                    <input
-                      :id="check"
-                      type="checkbox"
-                      :value="check"
-                      v-model="checkboxSelected"
-                      name="default-checkbox"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
-                    />
-                    <label
-                      :for="check"
-                      class="ml-2 text-sm font-medium text-gray-900"
-                      >{{ check }}</label
-                    >
-                  </div>
-                </li>
-              </ul>
-            </div>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 9l-7 7-7-7"
+              ></path>
+            </svg>
+          </button>
 
-            <button
-              type="button"
-              @click="clearButton"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+          <!-- Dropdown menu -->
+          <div
+            id="dropdownDefaultCheckbox"
+            class="hidden z-10 w-48 bg-white rounded divide-y divide-gray-100 shadow mt-2 lg:mt-0"
+            data-popper-reference-hidden=""
+            data-popper-escaped=""
+            data-popper-placement="bottom"
+            style="
+              inset: 0px auto auto 0px;
+              margin: 0px;
+              transform: translate3d(0px, 320px, 0px);
+            "
+          >
+            <ul
+              class="p-3 space-y-3 text-sm text-gray-700"
+              aria-labelledby="dropdownCheckboxButton"
             >
-              clear
-            </button>
+              <li v-for="check in dropdown">
+                <div class="flex items-center">
+                  <input
+                    :id="check"
+                    type="checkbox"
+                    :value="check"
+                    v-model="checkboxSelected"
+                    name="default-checkbox"
+                    class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
+                  />
+                  <label
+                    :for="check"
+                    class="ml-2 text-sm font-medium text-gray-900"
+                  >
+                    {{ check }}
+                  </label>
+                </div>
+              </li>
+            </ul>
           </div>
 
-          <!-- Search -->
-          <div class="relative">
-            <div
-              class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-            >
-              <svg
-                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="table-search"
-              v-model="search"
-              class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Search for templates"
-            />
-          </div>
+          <button
+            type="button"
+            @click="clearButton"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none"
+          >
+            clear
+          </button>
         </div>
       </div>
-      <div class="grid grid-cols-4 gap-4 mx-6">
-        <div v-for="temp in filtered">
-          <TemplateCard :template="temp" />
-        </div>
+      <div class="flex flex-wrap lg:mx-4">
+        <TemplateCard v-for="temp in filtered" :template="temp" />
       </div>
     </div>
   </div>
